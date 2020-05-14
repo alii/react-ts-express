e3f0c1a22c2afb3864d0422316e838efbea472ed
Alistair Smith <alistairsmith01+git@gmail.com>
2020-05-14 22:54:31 +0100

    Minor improvements

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
-import path from 'path';
-import fs from 'fs';
 
-const builtFile = path.join(__dirname, '..', 'dist', 'index.html');
-const hasFile = fs.existsSync(builtFile);
+import { join } from 'path';
+import { existsSync } from 'fs';
+
+const builtFile = join(__dirname, '..', 'dist', 'index.html');
+const hasFile = existsSync(builtFile);
 
 const app = express();
 app.use(express.static('dist'));
 
-app.get('/api/aaa', (req, res) => res.json('gung'));
+app.get('/backend-route', (req, res) => {
+  res.json({ hello: 'world', query: req.query });
+});
 
 app.get('*', (req, res) => {
   if (hasFile) {
